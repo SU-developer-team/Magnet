ba0baf7c9cfb3e1e203ca3cbf64154149f80ab97
Yerlan <www.aiapov@gmail.com>
2024-10-23 14:47:41 +0500

    Vertical logic

--- models.py
+++ models.py
@@ -1,11 +1,10 @@
-import math 
-
+import math
 
 class Magnet:
     def __init__(self, mass, diameter, height):
-        self.mass = mass
-        self.diameter = diameter
-        self.height = height
+        self.mass = mass  # Масса магнита (кг)
+        self.diameter = diameter  # Диаметр магнита (м)
+        self.height = height  # Высота магнита (м)
         self.radius = diameter / 2
 
     def magnetic_induction(self, z):
@@ -24,10 +23,61 @@ class Magnet:
             )
 
         return Bz(z, R, L)
-    
-    def coordinate_params():
-        pass
-    
+
+    def motion(self, F_external_func, z0, v0, t_total, time_step=0.001, z_min=-0.05, z_max=0.05):
+        """
+        Расчет ускорения, скорости и позиции магнита во времени под действием внешней силы и силы тяжести.
+        Теперь магнит ограничен границами z_min и z_max.
+        
+        F_external_func - функция, возвращающая внешнюю силу в зависимости от времени, положения и скорости
+        z0 - начальное положение (м)
+        v0 - начальная скорость (м/с)
+        t_total - общее время моделирования (с)
+        time_step - шаг по времени (с)
+        z_min - нижняя граница по оси z
+        z_max - верхняя граница по оси z
+        """
+        times = []
+        positions = []
+        velocities = []
+        accelerations = []
+
+        t = 0
+        z = z0
+        v = v0
+        m = self.mass
+        g = 9.81  # Ускорение свободного падения (м/с^2)
+
+        while t <= t_total:
+            # Получаем внешнюю силу в текущий момент времени
+            F_external = F_external_func(t, z, v)
+
+            # Расчет ускорения
+            a = (F_external - m * g) / m
+
+            # Обновление скорости и позиции (метод Эйлера)
+            v += a * time_step
+            z += v * time_step
+
+            # Проверка на границы и отражение
+            if z >= z_max:
+                z = z_max
+                v = -v  # Инвертируем скорость
+            elif z <= z_min:
+                z = z_min
+                v = -v  # Инвертируем скорость
+
+            # Сохранение данных
+            times.append(t)
+            positions.append(z)
+            velocities.append(v)
+            accelerations.append(a)
+
+            # Обновление времени
+            t += time_step
+
+        return times, positions, velocities, accelerations
+
 
 class Coil:
     def __init__(self, turns, radius, position=0):
@@ -36,50 +86,33 @@ class Coil:
         self.area = math.pi * radius**2
         self.position = position  # Положение катушки по оси z
 
-
-def calculate_emf(magnet, coil, velocities, z_starts, z_ends, time_step=0.001):
+def calculate_emf(magnet, coil, times, positions):
     """
-    Расчет ЭДС, индуцированной в катушке при прохождении магнита в нескольких фазах движения.
-    velocities - список скоростей для каждой фазы
-    z_starts - список начальных позиций для каждой фазы
-    z_ends - список конечных позиций для каждой фазы
+    Расчет ЭДС, индуцированной в катушке при движении магнита.
+    times - список времен
+    positions - список позиций магнита
     """
-    z_positions = []
     emfs = []
-    times = []
-    total_time = 0
+    Phi_prev = None
 
-    for v, z_start, z_end in zip(velocities, z_starts, z_ends):
-        z = z_start
-        t = total_time
+    for i in range(len(times)):
+        t = times[i]
+        z = positions[i]
 
-        Phi_prev = None
+        # Сдвигаем положение относительно катушки
+        z_rel = z - coil.position
 
-        # Определяем направление движения
-        direction = 1 if z_end > z_start else -1
+        B = magnet.magnetic_induction(z_rel)
+        Phi = coil.turns * B * coil.area
 
-        while (direction * z) <= (direction * z_end):
-            # Сдвигаем положение относительно катушки
-            z_rel = z - coil.position
+        if Phi_prev is not None:
+            dt = times[i] - times[i - 1]
+            dPhi_dt = (Phi - Phi_prev) / dt
+            emf = -dPhi_dt
+        else:
+            emf = 0
 
-            B = magnet.magnetic_induction(z_rel)
-            Phi = coil.turns * B * coil.area
+        emfs.append(emf)
+        Phi_prev = Phi
 
-            if Phi_prev is not None:
-                dPhi_dt = (Phi - Phi_prev) / time_step
-                emf = -dPhi_dt
-            else:
-                emf = 0
-
-            emfs.append(emf)
-            times.append(t)
-            z_positions.append(z)
-
-            Phi_prev = Phi
-            z += v * time_step
-            t += time_step
-
-        total_time = t  # Обновляем общее время для следующей фазы
-
-    return times, emfs, z_positions
-
+    return emfs
